417026f6d6437e0523b5b5645801f0084182abc5
or man <Ormn1996@gmail.com>
2022-06-06 11:18:56 +0300

    css

--- gol-front/src/app/grid/grid.component.ts
+++ gol-front/src/app/grid/grid.component.ts
@@ -18,7 +18,7 @@ export class GridComponent implements OnInit {
     this.game.mat[0][0] = !this.game.mat[0][0]
   }
 
-  private size = `min(80vw/${this.game.grid_size},90vh/${this.game.grid_size})`
+  private size = `min(80vw/${this.game.grid_size},80vh/${this.game.grid_size})`
 
   public  cellCss = {
     'min-width': this.size
